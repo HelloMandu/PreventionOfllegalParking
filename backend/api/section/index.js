ed15eb70a4e6b395a66254a9b58b722695772dd2
HelloMandu <tjdals6695@gmail.com>
2020-09-25 01:11:58 +0900

    ExcexToJSON Complete

--- backend/api/section/index.js
+++ backend/api/section/index.js
@@ -1,5 +1,6 @@
 const Router = require("koa-router");
 const fs = require("fs");
+const ExcelToJSON = require('../../lib/ExcelToJSON');
 const XLSX = require("xlsx");
 const geolib = require("geolib");
 
@@ -7,7 +8,8 @@ const api = new Router();
 
 api.get("/:filename", (ctx) => {
     const {filename} = ctx.params;
-    let buf = fs.readFileSync(`../../data/xlsx/spot.xlsx`);
+    const result = ExcelToJSON(filename);
+    // let buf = fs.readFileSync(`../data/xlsx/spot.xlsx`);
     // let wb = XLSX.read(buf, { type: "buffer" });
     // let records = [];
     // wb.SheetNames.forEach((sheetName) => {
--- backend/lib/ExcelToJSON.js
+++ backend/lib/ExcelToJSON.js
@@ -1,46 +1,21 @@
-// // const fs = require("fs");
-// // const XLSX = require("xlsx");
+const fs = require("fs");
+const XLSX = require("xlsx");
 
-// // module.exports = function (filename) {
-// //   console.log(filename)
-// //   const LOCATION = `../data/xlsx/${filename}.xlsx`;
-// //   let buf = fs.readFileSync(LOCATION);
-// //   let wb = XLSX.read(buf, { type: "buffer" });
-// //   let result = [];
-// //   wb.SheetNames.forEach((sheetName) => {
-// //     result.push(XLSX.utils.sheet_to_json(wb.Sheets[sheetName]));
-// //   });
-// //   return result;
-// // };
-// const fs = require('fs');
-// const XLSX = require('xlsx');
+module.exports = function (filename) {
+  const LOCATION = `C:/Users/Sungmin/Desktop/4-2/Final/PreventionOfllegalParking/backend/data/xlsx/${filename}.xlsx`;
+  const buf = fs.readFileSync(LOCATION);
+  const wb = XLSX.read(buf, { type: "buffer" });
+  const json = {};
+  wb.SheetNames.forEach((sheetName) => {
+    json[sheetName] = XLSX.utils.sheet_to_json(wb.Sheets[sheetName])
+  });
+  return json;
+};
+// const fs = require('fs')
+// const XLSX = require('xlsx')
 
-// function test(filename)  {
-//   // const LOCATION = `../data/xlsx/${filename}.xlsx`;
-//   const LOCATION = `test.txt`;
-//   if(fs.existsSync(LOCATION)){
-//     console.log("trye");
-//   }
-//   else{
-//     console.log("왜ㅅㅂ");
-//   }
-//   // let buf = fs.readFileSync(LOCATION);
-//   // let wb = XLSX.read(buf, { type: "buffer" });
-//   // let result = [];
-//   // wb.SheetNames.forEach((sheetName) => {
-//   //   result.push(XLSX.utils.sheet_to_json(wb.Sheets[sheetName]));
-//   // });
-//   // // return result;
-//   // console.log(result)
-//   // fs.readdir('../data/xlsx',function(err,filelist){ console.log(filelist); });
-
-// };
-// test("spot");
-const fs = require('fs')
-const XLSX = require('xlsx')
-
-let buf = fs.readFileSync('./spot.xlsx')
-let wb = XLSX.read(buf, { type: 'buffer' })
+// let buf = fs.readFileSync('C:/Users/Sungmin/Desktop/4-2/Final/PreventionOfllegalParking/backend/data/xlsx/spot.xlsx')
+// let wb = XLSX.read(buf, { type: 'buffer' })
 
 // let sheetName = wb.SheetNames
 // let sheet = wb.Sheets
@@ -48,9 +23,9 @@ let wb = XLSX.read(buf, { type: 'buffer' })
 //   let rows = XLSX.utils.sheet_to_json(sheet[name])
 //   console.log(rows)
 // })
-wb.SheetNames.forEach((sheetName) => {
-  console.log('sheetName: ' + sheetName)
-
-  let rows = XLSX.utils.sheet_to_json(wb.Sheets[sheetName])
-  console.log(rows)
-})
+// const json = {};
+// wb.SheetNames.forEach((sheetName) => {
+//   console.log(sheetName);
+//   json[sheetName] = XLSX.utils.sheet_to_json(wb.Sheets[sheetName])
+// })
+// console.log(json.crosswalk);
